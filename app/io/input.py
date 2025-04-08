3e284aeaca742b293ecbd84018a4bff3cfb36a8c
Nikita Kostenko <nikkost.ua@gmail.com>
2025-04-08 20:19:56 +0300

    Fulled files input.py and output.py with empty functions and docstr to them

---
+++ app/io/input.py
@@ -0,0 +1,22 @@
+def input_from_console():
+    """
+    Reads info input from console
+    :return: user's input as a String
+    """
+    pass
+
+def read_file_builtin():
+    """
+    Reads text from a file using Python built-in func
+    :param: Path to the file
+    :return: file content as a string
+    """
+    pass
+
+def read_file_pandas():
+    """
+    Reads data from a file using pandas library
+    :param: Path to the file
+    :return: file content as a data frame
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,16 @@
+def print_to_console():
+    """
+    Prints given txt to console
+    :param: text to be written
+    :return: printed text
+    """
+    pass
+
+def write_to_file():
+    """
+    Writes txt to file using built-i features
+    :param: Path to the file
+    :param: text
+    :return: txt to file
+    """
+    pass
